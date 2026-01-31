2fef5993820cecb0f13fec8e8778734a79ad5506
Elmicapn <155172963+Elmicapn@users.noreply.github.com>
2026-01-31 14:15:37 +0100

    Add crackme01

--- crackme_01/README.md
+++ crackme_01/README.md
@@ -1,3 +1,39 @@
-# Crakme 01
+# Crackme 01
 
-# Niveau
+## Niveau : Facile
+
+### Description
+
+Ce crackme est un programme écrit en assembleur x86 (32 bits) pour Linux.
+Il lit un mot de passe depuis l’entrée standard et vérifie sa validité.
+
+Le mot de passe attendu fait **exactement 16 caractères**.
+Aucun mot de passe n’est stocké en clair dans le binaire.
+
+Chaque caractère saisi est transformé par une opération arithmétique simple
+avant d’être comparé à une valeur interne.
+
+
+### Fonctionnement
+
+- Le programme lit l’entrée utilisateur depuis `stdin`
+- Chaque caractère est traité de manière indépendante
+- Une transformation **(+1)** est appliquée à chaque octet
+- Les valeurs transformées sont comparées en boucle
+- Si une seule comparaison échoue, l’accès est refusé
+
+Le programme accepte soit :
+- un caractère de fin de ligne (`\\n`)
+- soit une fin de chaîne (`\\0`)
+après les 16 caractères du mot de passe.
+
+### Contraintes techniques
+
+- Architecture : x86 (32 bits)
+- Système : Linux
+- Langage : assembleur NASM
+
+### Utilisation
+
+```bash
+$ echo "PASSWORD" | ./crackme01
---
+++ crackme_01/crackme01.s
@@ -0,0 +1,68 @@
+global _start
+
+section .data
+    msg_good db "Bien joué, C'est un bon debut pas vrai ?", 10
+    len_good equ $ - msg_good
+
+    msg_bad  db "Continue tu vas y arriver !", 10
+    len_bad  equ $ - msg_bad
+
+    mss db \
+        0x56,0x4F,0x60,0x43,0x50,0x4F,0x60,0x45, \
+        0x46,0x43,0x56,0x55,0x60,0x57,0x53,0x4E
+
+section .bss
+    input resb 32
+
+section .text
+
+_start:
+    mov eax, 3 
+    mov ebx, 0          
+    mov ecx, input
+    mov edx, 32
+    int 0x80
+
+    mov esi, input
+
+
+    xor edi, edi 
+    mov ecx, 16  
+
+check_loop:
+    mov al, [esi + edi] 
+    add al, 1              
+    cmp al, [mss + edi]
+    jne npasswd
+
+    inc edi
+    loop check_loop
+
+    cmp byte [esi + 16], 0x0a
+    je bpasswd
+    cmp byte [esi + 16], 0x00
+    je bpasswd
+
+    jmp npasswd
+
+bpasswd:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg_good
+    mov edx, len_good
+    int 0x80
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+npasswd:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg_bad
+    mov edx, len_bad
+    int 0x80
+
+    mov eax, 1
+    mov ebx, 1
+    int 0x80
